988af644c693d71e1326073ddbe4b338575585b8
giuliagiannetti <108627072+giuliagiannetti@users.noreply.github.com>
2023-01-22 18:46:01 +0100

    ora abbiamo una scherminiz yeee

--- src/scenes/scene0_welcome.js
+++ src/scenes/scene0_welcome.js
@@ -18,6 +18,10 @@ export default class WelcomeScene extends Phaser.Scene {
 
       //sfondo
       this.load.image("background_base", "assets/images/background/schermata_iniziale_render.jpg");
+      //titolo
+      this.load.image("titolo", "assets/images/text/titolo.png");
+      //testo crediti
+      this.load.image("testo_crediti", "assets/images/text/testoCrediti@4x.png");
 
       //bottoni con sprite (falliti)
       /*this.load.spritesheet( 'bottoneGioca' , 'assets/images/buttons/spriteGioca.png' ,{frameWidth:191,frameHeight:82});
@@ -49,7 +53,12 @@ export default class WelcomeScene extends Phaser.Scene {
       //posizione degli elementi 
       this.background = this.add.image(this.game.config.width / 2, this.game.config.height / 2, "background_base");
       this.background.setOrigin(0.5, 0.5);
-
+      this.titolo = this.add.image(80, 55, "titolo");
+      this.titolo.setOrigin(0, 0);
+      this.titolo.setScale(1.03);
+      this.testoCrediti = this.add.image(983, 451, "testo_crediti");
+      this.testoCrediti.setOrigin(0, 0);
+      this.testoCrediti.setScale(0.25); //ho dovuto fare un file quattro volte più grande e poi scalarlo se no veniva pixelato
       
 
       //immagine del bottone
@@ -58,12 +67,12 @@ export default class WelcomeScene extends Phaser.Scene {
       this.bottone_storia = this.add.sprite(300,342,'bottoneStoria');
       this.bottone_crediti = this.add.sprite(300,442,'bottoneCrediti');*/
       
-      this.bottone_gioca = this.add.image(300,242,'bottoneGioca');
-      this.bottone_storia = this.add.image(300,342,'bottoneStoria');
-      this.bottone_crediti = this.add.image(300,442,'bottoneCrediti');
-      this.bottone_gioca_hover = this.add.image(300,242,'bottoneGioca_hover');
-      this.bottone_storia_hover = this.add.image(300,342,'bottoneStoria_hover');
-      this.bottone_crediti_hover = this.add.image(300,441,'bottoneCrediti_hover');
+      this.bottone_gioca = this.add.image(127,319,'bottoneGioca');
+      this.bottone_storia = this.add.image(127,442,'bottoneStoria');
+      this.bottone_crediti = this.add.image(127,565,'bottoneCrediti');
+      this.bottone_gioca_hover = this.add.image(127,319,'bottoneGioca_hover');
+      this.bottone_storia_hover = this.add.image(127,442,'bottoneStoria_hover');
+      this.bottone_crediti_hover = this.add.image(127,565,'bottoneCrediti_hover');
 
 
       //origini, interattività, trasparenza
@@ -91,21 +100,21 @@ export default class WelcomeScene extends Phaser.Scene {
       this.bottone_crediti.setVisible(true);
 
 
-      this.crediti = this.add.rectangle(300,442, 191, 81, 0x000000, 0);
+      this.crediti = this.add.rectangle(127,565, 191, 81, 0x000000, 0);
       this.crediti.setOrigin(0,0);
       this.crediti.setInteractive();
 
-      this.storia = this.add.rectangle(300,342, 191, 81, 0x000000, 0);
+      this.storia = this.add.rectangle(127,442, 191, 81, 0x000000, 0);
       this.storia.setOrigin(0,0);
       this.storia.setInteractive();
 
-      this.gioca = this.add.rectangle(300,242, 191, 81, 0x000000, 0);
+      this.gioca = this.add.rectangle(127,319, 191, 81, 0x000000, 0);
       this.gioca.setOrigin(0,0);
       this.gioca.setInteractive();
 
       
 
-//bottoni interattivi con trasparenza
+//effetto di hover con trasparenza
    this.gioca.on('pointerover',() => {
       this.bottone_gioca_hover.setVisible(true);
       this.bottone_gioca.setVisible(false);
