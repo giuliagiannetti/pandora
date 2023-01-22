e25cd8fd44143b6cfa44beb280677b329be32c2a
giuliagiannetti <108627072+giuliagiannetti@users.noreply.github.com>
2023-01-22 13:34:28 +0100

    the fucking buttons man

--- src/scenes/scene0_welcome.js
+++ src/scenes/scene0_welcome.js
@@ -17,12 +17,26 @@ export default class WelcomeScene extends Phaser.Scene {
       //assets
 
       //sfondo
-      this.load.image("background_base", "assets/images/background/scherminiz_render_bozza1.jpg");
+      this.load.image("background_base", "assets/images/background/schermata_iniziale_render.jpg");
 
-      //componenti grafiche (button)
-      this.load.image("bottoneGioca", "assets/images/buttons/BottoneGioca.png");
+      //bottoni con sprite (falliti)
+      /*this.load.spritesheet( 'bottoneGioca' , 'assets/images/buttons/spriteGioca.png' ,{frameWidth:191,frameHeight:82});
+      this.load.spritesheet( 'bottoneStoria' , 'assets/images/buttons/spriteStoria.png' ,{frameWidth:191,frameHeight:82});
+      this.load.spritesheet( 'bottoneCrediti' , 'assets/images/buttons/spriteCrediti.png' ,{frameWidth:191,frameHeight:82});*/
+
+      //bottoni con immagini normali
+      this.load.image("bottoneGioca", "assets/images/buttons/bottoneGioca.png");
+      this.load.image("bottoneStoria", "assets/images/buttons/bottoneStoria.png");
+      this.load.image("bottoneCrediti", "assets/images/buttons/bottoneCrediti.png");
+        //bottoni con hover
+        this.load.image("bottoneGioca_hover", "assets/images/buttons/bottoneGioca_hover.png");
+        this.load.image("bottoneStoria_hover", "assets/images/buttons/bottoneStoria_hover.png");
+        this.load.image("bottoneCrediti_hover", "assets/images/buttons/bottoneCrediti_hover.png");
+      
+      //bottoni scorsi
+      /*this.load.image("bottoneGioca", "assets/images/buttons/BottoneGioca.png");
       this.load.image("bottoneStoria", "assets/images/buttons/BottoneStoria.png");
-      this.load.image("bottoneCrediti", "assets/images/buttons/BottoneCrediti.png");
+      this.load.image("bottoneCrediti", "assets/images/buttons/BottoneCrediti.png");*/
 
    };
 
@@ -36,10 +50,106 @@ export default class WelcomeScene extends Phaser.Scene {
       this.background = this.add.image(this.game.config.width / 2, this.game.config.height / 2, "background_base");
       this.background.setOrigin(0.5, 0.5);
 
-      this.background.setPipeline('Light2D').setAlpha(0.5);
+      
 
       //immagine del bottone
-      this.playbutton = this.add.image(300, 242, "bottoneGioca");
+        //con sprite(fallito)
+      /*this.bottone_gioca = this.add.sprite(300,242,'bottoneGioca');
+      this.bottone_storia = this.add.sprite(300,342,'bottoneStoria');
+      this.bottone_crediti = this.add.sprite(300,442,'bottoneCrediti');*/
+      
+      this.bottone_gioca = this.add.image(300,242,'bottoneGioca');
+      this.bottone_storia = this.add.image(300,342,'bottoneStoria');
+      this.bottone_crediti = this.add.image(300,442,'bottoneCrediti');
+      this.bottone_gioca_hover = this.add.image(300,242,'bottoneGioca_hover');
+      this.bottone_storia_hover = this.add.image(300,342,'bottoneStoria_hover');
+      this.bottone_crediti_hover = this.add.image(300,442,'bottoneCrediti_hover');
+
+      //origini, interattività, trasparenza
+      this.bottone_gioca.setOrigin(0, 0);
+      this.bottone_gioca.setInteractive();
+      this.bottone_gioca.setAlpha(1);
+
+      this.bottone_storia.setOrigin(0, 0);
+      this.bottone_storia.setInteractive();
+      this.bottone_storia.setAlpha(1);
+
+      this.bottone_crediti.setOrigin(0, 0);
+      this.bottone_crediti.setInteractive();
+      this.bottone_crediti.setAlpha(1);
+
+
+      this.bottone_gioca_hover.setOrigin(0, 0);
+      this.bottone_gioca_hover.setInteractive();
+      this.bottone_gioca_hover.setAlpha(0);
+
+      this.bottone_storia_hover.setOrigin(0, 0);
+      this.bottone_storia_hover.setInteractive();
+      this.bottone_storia_hover.setAlpha(0);
+
+      this.bottone_crediti_hover.setOrigin(0, 0);
+      this.bottone_crediti_hover.setInteractive();
+      this.bottone_crediti_hover.setAlpha(0);
+
+//bottoni interattivi con trasparenza
+this.bottone_gioca.on('pointerover',() => {
+   this-bottone_gioca.setAlpha(0);
+   this.bottone_gioca_hover.setAlpha(1);
+});
+
+this.bottone_gioca.on('pointerout',() => {
+   this.bottone_gioca.setAlpha(1);
+   this.bottone_gioca_hover.setAlpha(0);
+});
+
+this.bottone_storia.on('pointerover',function(pointer){
+   this.bottone_storia.setAlpha(0);
+   this.bottone_storia_hover.setAlpha(1);
+});
+
+this.bottone_storia.on('pointerout',function(pointer){
+   this.bottone_storia.setAlpha(1);
+   this.bottone_storia_hover.setAlpha(0);
+});
+
+this.bottone_crediti.on('pointerover',function(pointer){
+   this.bottone_crediti.setAlpha(0);
+   this.bottone_crediti_hover.setAlpha(1);
+});
+
+this.bottone_crediti.on('pointerout',function(pointer){
+   this.bottone_crediti.setAlpha(1);
+   this.bottone_crediti_hover.setAlpha(0);
+});
+
+
+      //bottoni interattivi con spritesheet (fallito)
+/*bottone_gioca.on('pointerover',() => {
+    bottone_gioca.setFrame(1);
+});
+
+bottone_gioca.on('pointerout',() => {
+    bottone_gioca.setFrame(0);
+});
+
+bottone_storia.on('pointerover',function(pointer){
+   bottone_storia.setFrame(1);
+})
+
+bottone_storia.on('pointerout',function(pointer){
+   bottone_storia.setFrame(0);
+})
+
+bottone_crediti.on('pointerover',function(pointer){
+   bottone_crediti.setFrame(1);
+})
+
+bottone_crediti.on('pointerout',function(pointer){
+   bottone_crediti.setFrame(0);
+})*/
+      
+      //gl scorsi bottoni
+      /*this.playbutton = this.add.image(300, 242, "bottoneGioca");
       this.bottone_storia = this.add.image(300, 392, "bottoneStoria");
       this.bottone_crediti = this.add.image(300, 542, "bottoneCrediti");
 
@@ -65,8 +175,10 @@ export default class WelcomeScene extends Phaser.Scene {
       //bottone cliccabile 
       this.bottone_crediti.on("pointerdown", () => {
          this.scene.start("scene_credits");
-      });
+      });*/
 
+      //luce che abbiamo deciso di togliere credo
+      /*this.background.setPipeline('Light2D').setAlpha(0.5);
       var light = this.lights.addLight(300, 300, 1300).setScrollFactor(0.0).setIntensity(3);
 
       this.lights.enable();
@@ -77,10 +189,7 @@ export default class WelcomeScene extends Phaser.Scene {
          light.x = pointer.x;
          light.y = pointer.y;
 
-      });
-
-
-
+      });*/
    };
 
 
