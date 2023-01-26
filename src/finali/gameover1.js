dca2662951958c4f984c9afc7541e1d418794c6d
cate <113889043+hekateiss@users.noreply.github.com>
2023-01-26 13:49:49 +0100

    debug + tasti in italiano

--- src/finali/gameover1.js
+++ src/finali/gameover1.js
@@ -13,7 +13,7 @@ preload(){
     this.load.image("gameover", "assets/images/story/gameover.jpg");
     this.load.image("home","assets/images/buttons/home_text.png");
     this.load.image("restart","assets/images/buttons/restart_text_1.png");
-    this.load.image("testoBad", "assets/images/story/testo_bad.png")
+    this.load.image("testoBad", "assets/images/story/testo_bad.png");
 }
 
 create(){
